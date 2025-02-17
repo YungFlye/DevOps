7668b57e78f41f0a150d83827b606dd903a1f029
YungFlye <70700090+YungFlye@users.noreply.github.com>
2025-02-17 11:08:27 +0100

    Feature/lint and prettier (#24)
    * test
    
    * added prettier and lint
    
    * fixed eslint
    
    * removed pr validator file
    
    * ran prettier write
    
    ---------
    
    Co-authored-by: KennethFlye <10434642@ucn.dk>

--- api/squareroot.js
+++ api/squareroot.js
@@ -1,13 +1,13 @@
-var router = require('express').Router();
+const router = require('express').Router();
 
 /* GET squareroot of a number */
 router.get('/', (req, res, next) => {
-    let input = req.query.input;
-    let result = Math.sqrt(input);
+  const input = req.query.input;
+  const result = Math.sqrt(input);
 
-    let output = Math.round(result);
+  const output = Math.round(result);
 
-    res.send({ "result": output});
+  res.send({ result: output });
 });
 
 module.exports = router;
--- tests/squareroot.test.js
+++ tests/squareroot.test.js
@@ -1,23 +1,22 @@
-const request = require('supertest')
+const request = require('supertest');
 const app = require('../app');
 
-
 describe('Sanity test', () => {
-    test('1 should equal 1', () => {
-        expect(1).toBe(1)
-    })
+  test('1 should equal 1', () => {
+    expect(1).toBe(1);
+  });
 });
 
 describe('addition test', () => {
-    test('2 + 2 should equal 2', () => {
-        expect(2+2).toBe(4)
-    })
+  test('2 + 2 should equal 2', () => {
+    expect(2 + 2).toBe(4);
+  });
 });
 
 describe('Squareroot endpoint', () => {
-    test('should return the squareroot of 7744', async () => {
-        const res = await request(app).get('/api/squareroot/?input=7744');
-        expect(res.statusCode).toEqual(200);
-        expect(res.body.result).toEqual(88);
-    });
+  test('should return the squareroot of 7744', async () => {
+    const res = await request(app).get('/api/squareroot/?input=7744');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.result).toEqual(88);
+  });
 });
